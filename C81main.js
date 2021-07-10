f1a171beb25c90f992ad4de856d1e61e3635d2a9
mayankshahi <mayank.shahi@gmail.com>
2021-07-10 15:14:20 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,53 @@
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d")
+
+color = "red";
+
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.linewidth = 2;
+ctx.arc(200,200,40,0,2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e){
+    color = document.getElementById("color").value;
+
+    mouse_x = e.clientX- canvas.offsetLeft;
+    mouse_y = e.clientY- canvas.offsetTop;
+
+    circle(mouse_x , mouse_y);
+
+    mouse_x = mouse_x-40;
+    mouse_y = mouse_y-40;
+
+    circle(mouse_x , mouse_y);
+
+    mouse_x = mouse_x-40;
+    mouse_y = mouse_y+40;
+    
+    circle(mouse_x , mouse_y);
+
+    mouse_x = mouse_x-40;
+    mouse_y = mouse_y-40;
+
+    circle(mouse_x , mouse_y);
+
+    mouse_x = mouse_x-40;
+    mouse_y = mouse_y+40;
+    
+    circle(mouse_x , mouse_y);
+}
+
+function circle(mouse_x , mouse_y){
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.linewidth = 2;
+    ctx.arc(mouse_x,mouse_y,40,0,2*Math.PI)
+    ctx.stroke();
+}
+function clearArea()
+{
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
